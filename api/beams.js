c66bf523ddfb328508c2cdd0080087436f55b727
braviomichel <sagbomichel08@gmail.com>
2024-03-24 20:06:51 +0100

    ubo chat app

--- api/beams.js
+++ api/beams.js
@@ -1,32 +1,27 @@
-import {getConnecterUser, triggerNotConnected} from "../lib/session";
+import { getConnecterUser, triggerNotConnected } from "../lib/session";
 
 const PushNotifications = require("@pusher/push-notifications-server");
 
-
 export default async (req, res) => {
+	const userIDInQueryParam = req.query["user_id"];
+	const user = await getConnecterUser(req);
+	if (
+		user === undefined ||
+		user === null ||
+		userIDInQueryParam !== user.externalId
+	) {
+		console.log("Not connected");
+		triggerNotConnected(res);
+		return;
+	}
 
-   const userIDInQueryParam = req.query["user_id"];
-    const user = await getConnecterUser(req);
-    console.log("PushToken : " + userIDInQueryParam + " -> " + JSON.stringify(user) + "external user id : " + user.externalId);
-    if (user === undefined || user === null || userIDInQueryParam !== user.externalId) {
-        console.log("Not connected");
-        triggerNotConnected(res);
-        return;
-    }
+	const beamsClient = new PushNotifications({
+		instanceId: "YOUR PUSHER KEY",
+		secretKey: "YOUR PUSHER KEY",
+	});
 
-    console.log("Using push instance : " + "14e90d90-e252-466d-a7f7-80087f6f7deb");
-    const beamsClient = new PushNotifications({
-        instanceId: "14e90d90-e252-466d-a7f7-80087f6f7deb",
-        secretKey: "8CE36D5FF6B841C1E110EC8449D430C76125B0D2696CC4FA55EB32FCFE8B96B5",
-        
-    });
+	const beamsToken = beamsClient.generateToken(user.externalId);
 
-    const beamsToken = beamsClient.generateToken(user.externalId);
-
-    console.log(JSON.stringify(beamsToken));
-    res.send(beamsToken);
-
-}
-
-
-
+	console.log(JSON.stringify(beamsToken));
+	res.send(beamsToken);
+};
